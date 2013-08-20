418b10c7afa5380bf9abc1fc4077873a39e8860c
mhSun <asmh1989@gmail.com>
2013-08-20 17:15:20 +0800

    add book detail

---
+++ mReader/src/com/sun/mybookreader/mt/MtBookDetail.java
@@ -0,0 +1,12 @@
+package com.sun.mybookreader.mt;
+
+import java.util.HashMap;
+
+import com.sun.mybookreader.html.LinkTagSet;
+
+public class MtBookDetail {
+	public String imageUrl;
+	public String bookDetail="";
+	public String bookAbout;
+	public HashMap<String, LinkTagSet> bookChapters = new HashMap<String, LinkTagSet>();
+}

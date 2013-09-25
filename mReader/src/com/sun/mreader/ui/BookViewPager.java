bc69274aef5e513e0f5d9f31f5462f39f5ccf678
mhSun <asmh1989@gmail.com>
2013-09-25 17:36:55 +0800

    update

---
+++ mReader/src/com/sun/mreader/ui/BookViewPager.java
@@ -0,0 +1,38 @@
+package com.sun.mreader.ui;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.sun.mreader.utils.Log;
+
+public class BookViewPager extends ViewPager {
+
+	private static final String TAG = "BookViewPager";
+	private boolean canScrollLeft = true;
+	private boolean canScrollRight = true;
+
+
+	public void setScrollLeft(boolean b){
+		canScrollLeft = b;
+	}
+
+	public void setScrollRight(boolean b){
+		canScrollRight = b;
+	}
+
+	float mLastMotionX;
+	float mLastMotionY;
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent arg0) {
+
+		return super.onInterceptTouchEvent(arg0);
+	}
+	public BookViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+}
